d31821ea517a7547464a14504d6ddbb8037a2e18
Jeff Stevens <jeff@stevens.net>
2018-12-02 12:02:49 -0500

    Add files via upload

---
+++ removeapps.ps1
@@ -0,0 +1,59 @@
+#3D Builder: 
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+
+#Alarms and Clock: 
+Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+
+#Calculator: 
+Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+
+#Calendar and Mail: 
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+
+#Camera: 
+Get-AppxPackage *windowscamera* | Remove-AppxPackage
+
+#Get Office: 
+Get-AppxPackage *officehub* | Remove-AppxPackage
+
+#Get Skype: 
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+
+#Get Started: 
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+
+#Groove Music: 
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+
+#Maps: 
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+
+#Microsoft Solitaire Collection: 
+Get-AppxPackage *solitairecollection* | Remove-AppxPackage
+
+#Money: 
+Get-AppxPackage *bingfinance* | Remove-AppxPackage
+
+#Movies & TV: 
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+
+#News: 
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+
+#People: 
+Get-AppxPackage *people* | Remove-AppxPackage
+
+#Phone Companion: 
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+
+#Photos: 
+Get-AppxPackage *photos* | Remove-AppxPackage
+
+#Sports: 
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+
+#Weather: 
+Get-AppxPackage *bingweather* | Remove-AppxPackage
+
+#Xbox: 
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
